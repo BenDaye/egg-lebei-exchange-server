0f6083016acea2073c1b0e9470a90610f69e86e1
BenDaye <yypsgdsg@163.com>
2021-06-20 22:44:26 +0800

    feat: enable cache

--- config/config.prod.js
+++ config/config.prod.js
@@ -34,6 +34,72 @@ module.exports = () => {
     passportJwt: {
       secret: process.env.PASSPORT_JWT_SECRET,
     },
+    lruCache: {
+      clients: {
+        ccxt: {
+          maxAge: 1000 * 2,
+        },
+        cmc: {},
+        juhe: {},
+      },
+      default: {
+        max: 500,
+        maxAge: 1000 * 60 * 60,
+      },
+      app: true,
+    },
+    cacheManager: {
+      clients: {
+        mongooseCcxt: {
+          store: 'mongoose',
+          options: {
+            modelName: 'CacheCcxt',
+            modelOptions: {
+              collection: 'cache_ccxt',
+            },
+          },
+          ttl: 2,
+          auth: 'cache:cache123698745',
+        },
+        redisCcxt: {
+          store: 'redis',
+          db: 0,
+          port: 6379,
+          ttl: 2,
+        },
+        mongooseCMC: {
+          store: 'mongoose',
+          options: {
+            modelName: 'CacheCMC',
+            modelOptions: {
+              collection: 'cache_cmc',
+            },
+          },
+          auth: 'cache:cache123698745',
+        },
+        mongooseJuhe: {
+          store: 'mongoose',
+          options: {
+            modelName: 'CacheJuhe',
+            modelOptions: {
+              collection: 'cache_juhe',
+            },
+          },
+          auth: 'cache:cache123698745',
+        },
+        memory: {
+          store: 'memory',
+        },
+      },
+      default: {
+        ttl: 60 * 60,
+        max: 500,
+        db: 'lebei',
+        host: 'localhost',
+        port: 27017,
+      },
+      app: true,
+    },
   };
 
   return { ...config, ...userConfig };
--- config/plugin.prod.js
+++ config/plugin.prod.js
@@ -1,9 +1,19 @@
 'use strict';
 
+const path = require('path');
+
 /** @type Egg.EggPlugin */
 module.exports = {
   cors: {
     enable: true,
     package: 'egg-cors',
+  },
+  lruCache: {
+    enable: true,
+    path: path.join(__dirname, '../lib/plugin/egg-lru-cache'),
+  },
+  cacheManager: {
+    enable: true,
+    path: path.join(__dirname, '../lib/plugin/egg-cache-manager'),
   },
 };
